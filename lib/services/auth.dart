02665119bdc4c7e6619c22b0e1f69182872c686b
Samuel Otterman <sjotterman@gmail.com>
2020-03-29 18:25:37 -0400

    basic user class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_firebase/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user object based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser fbUser) {
+    return fbUser != null ? User(uid: fbUser.uid) : null;
+  }
 
   // sign in anonymously
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
