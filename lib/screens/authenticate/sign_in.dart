6a195e7cf0c339e46e72e1bad7aab654cc33ad59
Samuel Otterman <sjotterman@gmail.com>
2020-03-31 13:34:52 -0400

    sign in with email and password

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -11,10 +11,12 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -35,30 +37,48 @@ class _SignInState extends State<SignIn> {
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
+              key: _formKey,
               child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(onChanged: (val) {
-                setState(() => email = val);
-              }),
-              SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text('Sign In', style: TextStyle(color: Colors.white)),
-                onPressed: () async {
-                  print(email);
-                  print(password);
-                },
-              )
-            ],
-          ))),
+                children: <Widget>[
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      }),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    obscureText: true,
+                    validator: (val) => val.length < 6
+                        ? 'Password must be 6+ characters'
+                        : null,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  RaisedButton(
+                    color: Colors.pink[400],
+                    child:
+                        Text('Sign In', style: TextStyle(color: Colors.white)),
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        print('valid');
+                        dynamic result = await _auth.signInWithEmailAndPassword(
+                            email, password);
+                        if (result == null) {
+                          setState(() {
+                            error = 'Could not sign in with these credentials';
+                          });
+                        }
+                      }
+                    },
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(error,
+                      style: TextStyle(color: Colors.red, fontSize: 14.0))
+                ],
+              ))),
     );
   }
 }
