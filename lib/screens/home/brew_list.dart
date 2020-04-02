3afbd8bd521d4d9260270aed78203a8778e725a3
Samuel Otterman <sjotterman@gmail.com>
2020-04-02 13:57:02 -0400

    widget for displaying items

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/screens/home/brew_tile.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_firebase/models/brew.dart';
 
@@ -11,11 +12,11 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
-    brews.forEach((brew) {
-      print(brew.name);
-      print(brew.sugars);
-      print(brew.strength);
-    });
-    return Container();
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
