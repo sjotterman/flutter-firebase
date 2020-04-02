c49ea29943853aee11ea33e287e6da99b8805027
Samuel Otterman <sjotterman@gmail.com>
2020-04-02 10:04:59 -0400

    initialize dummy data for new users

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_firebase/models/user.dart';
+import 'package:flutter_firebase/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -45,6 +46,9 @@ class AuthService {
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
+      // create a new document for the user with the uid
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'new crew member', 100);
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
---
+++ lib/services/database.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  // collection reference
+  final String uid;
+
+  DatabaseService({this.uid});
+  final CollectionReference brewCollection =
+      Firestore.instance.collection('brews');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection
+        .document(uid)
+        .setData({'sugars': sugars, 'name': name, 'strength': strength});
+  }
+}
