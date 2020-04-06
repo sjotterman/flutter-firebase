446b83a3d07a95c1a558103dafaaf4c5f328e473
Samuel Otterman <sjotterman@gmail.com>
2020-04-06 09:25:28 -0400

    finished app

--- lib/screens/home/brew_tile.dart
+++ lib/screens/home/brew_tile.dart
@@ -14,7 +14,10 @@ class BrewTile extends StatelessWidget {
         margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
         child: ListTile(
           leading: CircleAvatar(
-              radius: 25.0, backgroundColor: Colors.brown[brew.strength]),
+            radius: 25.0,
+            backgroundColor: Colors.brown[brew.strength],
+            backgroundImage: AssetImage('assets/coffee_icon.png'),
+          ),
           title: Text(brew.name),
           subtitle: Text('Takes ${brew.sugars} sugar(s)'),
         ),
